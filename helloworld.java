e2deb0c33dc4edbcf12ba878465d9d0fed27a21d
jayanthp <jayanthp@test.c.pwc-adv-dat-rainfall-proto-1.internal>
2015-11-19 15:17:55 +0000

    Commit 4

--- helloworld.java
+++ helloworld.java
@@ -3,7 +3,5 @@ public class helloworld
 	public static void main (String [] args) 
 	{
 		System.out.println("Hello World!");
-		System.out.println("Hello World 2");
-		System.out.println("Hello World 3");
 	}
 }
